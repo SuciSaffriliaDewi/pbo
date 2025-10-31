1b089f1a079b61552e1ede7c5cef0f2a927db17d
unknown <sucisaffrilia245@gmail.com>
2025-10-31 15:32:17 +0700

    Pertemuan ke 3

--- luasLingkaran.php
+++ luasLingkaran.php
@@ -1,14 +1,13 @@
 <?php
 
 class luasLingkaran {
-
+	
       public const phi = 3.14;
       public int $jari;
 }
 
 $lingkaran = new luasLingkaran();
-$lingkaran->jari = 10;
+$lingkaran->jari = 12;
 
 $rumus = luasLingkaran::phi * ($lingkaran->jari * $lingkaran->jari);
-
-echo "Hasilnya adalah: ". $rumus;
+echo "Hasilnya adalah: ".$rumus;
